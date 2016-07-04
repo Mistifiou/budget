c8cc1aa5a519cf197cfd0190e299aba9b38e6cee
sefo <fournierse@gmail.com>
2016-07-04 20:29:40 +0200

    refacto: données déplacées dans composant main + ajout du reste (revenus -dépenses)

--- app/components/budget-block/block.component.js
+++ app/components/budget-block/block.component.js
@@ -1,13 +1,14 @@
 var budgetBlock = {
     transclude: true,
     bindings: {
-        type: '@'
+        type: '@',
+        budget: '='
     },
     controller: BlockBudgetController,
     templateUrl: 'components/budget-block/block.template.html'
 };
 
-function BlockBudgetController(budgetService) {
+function BlockBudgetController() {
 
     var vm = this;
 
@@ -17,96 +18,41 @@ function BlockBudgetController(budgetService) {
         this.valeurModifiee = 0;
         this.addNew = false;
         this.newEntry = { id: null, type: "", intitule: "", valeur: null };
-        this.blockData = [];
-        this.budget = {
-            date: "07-2016",
-            revenus: [
-                {id: 1, type: "revenus", intitule: "Salaire-net", valeur: 1000},
-                {id: 2, type: "revenus", intitule: "CAF-APL", valeur: 400},
-                {id: 3, type: "revenus", intitule: "GP", valeur: 200}
-            ],
-            depensesRegulieres: [
-                {id: 1, type: "depensesRegulieres", intitule: "Loyer", valeur: 550},
-                {id: 2, type: "depensesRegulieres", intitule: "Essence", valeur: 60}
-            ],
-            depensesOccasionnelles: [
-                {id: 1, type: "depensesOccasionnelles", intitule: "Nourriture", valeur: 150}
-            ]
-        };
     };
 
     //initialisation données
     this.$postLink = function() {
-        if(this.type === "revenus") {
-            this.blockData = this.budget.revenus;
-           updateTotal("revenus");
-        }
-        else if(this.type === "depensesRegulieres") {
-            this.blockData = this.budget.depensesRegulieres;
-            updateTotal("depensesRegulieres");
-        }
-        else {
-            this.blockData = this.budget.depensesOccasionnelles;
-            updateTotal("depensesOccasionnelles");
-        }
+        this.total = getTotal(this.budget);
         closeModification();
     };
 
     //fonctions
     this.edit = function(budget, index) {
-        this.valeurModifiee = this.blockData[index].valeur;
+        this.valeurModifiee = this.budget[index].valeur;
         closeModification();
-        this.blockData[index].enModification = true;
+        this.budget[index].enModification = true;
     };
     this.update = function(valeur, index) {
-        this.blockData[index].valeur = valeur;
-        this.blockData[index].enModification = false;
-        if(this.blockData[index].type === "revenus")
-            updateTotal("revenus");
-        else if(this.blockData[index].type === "depensesRegulieres")
-            updateTotal("depensesRegulieres");
-        else
-            updateTotal("depensesOccasionnelles");
+        this.budget[index].valeur = valeur;
+        this.budget[index].enModification = false;
+        this.total = getTotal(this.budget);
     };
     this.delete = function(budget, index) {
-        if(budget.type === "revenus") {
-            this.budget.revenus.splice(index, 1);
-            updateTotal("revenus");
-        }
-        else if(budget.type === "depensesRegulieres") {
-            this.budget.depensesRegulieres.splice(index, 1);
-            updateTotal("depensesRegulieres");
-        }
-        else {
-            this.budget.depensesOccasionnelles.splice(index, 1);
-            updateTotal("depensesOccasionnelles");
-        }
+        this.budget.splice(index, 1);
+        this.total = getTotal(this.budget);
     };
     this.save = function() {
-        this.newEntry.id = this.blockData.length + 1;
-        this.newEntry.type = this.blockData[0].type;
-        this.blockData.push(this.newEntry);
+        this.newEntry.id = this.budget.length + 1;
+        this.newEntry.type = this.type;
+        this.budget.push(this.newEntry);
         this.newEntry = { id: null, type: "", intitule: "", valeur: null };
-        updateTotal(this.blockData[0].type);
+        this.total = getTotal(this.budget);
         this.addNew = false;
     };
-    this.getApi = function(param) {
-        budgetService.getTestApi(param).then(function(data) {
-            console.log(data);
-        });
-    };
     function closeModification() {
-        vm.blockData.forEach(function(data, index) {
+        vm.budget.forEach(function(data, index) {
             data.enModification = false;
         });
-    };
-    function updateTotal(type) {
-        if(type === "revenus")
-            vm.total = getTotal(vm.budget.revenus);
-        if(type === "depensesRegulieres")
-            vm.total = getTotal(vm.budget.depensesRegulieres);
-        if(type === "depensesOccasionnelles")
-            vm.total = getTotal(vm.budget.depensesOccasionnelles);
     };
     function getTotal(budgets) {
         var total = 0;
--- app/components/budget-main/budget.component.js
+++ app/components/budget-main/budget.component.js
@@ -4,17 +4,52 @@ var budgetMain = {
     templateUrl: 'components/budget-main/budget.template.html'
 };
 
-function BudgetMainController() {
+function BudgetMainController(budgetService) {
     //contructeur
     this.$onInit = function() {
         moment.locale('fr');
         this.displayDate = moment().format('MMMM YYYY');
+        this.budget = {
+            date: "07-2016",
+            revenus: [
+                {id: 1, type: "revenus", intitule: "Salaire-net", valeur: 1000},
+                {id: 2, type: "revenus", intitule: "CAF-APL", valeur: 400},
+                {id: 3, type: "revenus", intitule: "GP", valeur: 200}
+            ],
+            depensesRegulieres: [
+                {id: 1, type: "depensesRegulieres", intitule: "Loyer", valeur: 550},
+                {id: 2, type: "depensesRegulieres", intitule: "Essence", valeur: 60}
+            ],
+            depensesOccasionnelles: [
+                {id: 1, type: "depensesOccasionnelles", intitule: "Nourriture", valeur: 150}
+            ]
+        };
     };
     this.previous = function() {
         this.displayDate = moment(this.displayDate, 'MMMM YYYY').subtract(1, 'M').format('MMMM YYYY');
     };
     this.next = function() {
         this.displayDate = moment(this.displayDate, 'MMMM YYYY').add(1, 'M').format('MMMM YYYY');
+    };
+    this.getReste = function() {
+        var revenusTotal = 0;
+        var depensesOccasionnellesTotal = 0;
+        var depensesRegulieresTotal = 0;
+        this.budget.revenus.forEach(function(data) {
+            revenusTotal += parseInt(data.valeur, 10);
+        })
+        this.budget.depensesRegulieres.forEach(function(data) {
+            depensesRegulieresTotal += parseInt(data.valeur, 10);
+        })
+        this.budget.depensesOccasionnelles.forEach(function(data) {
+            depensesOccasionnellesTotal += parseInt(data.valeur, 10);
+        })
+        return revenusTotal - depensesOccasionnellesTotal - depensesRegulieresTotal;
+    };
+    this.getApi = function(param) {
+        budgetService.getTestApi(param).then(function(data) {
+            console.log(data);
+        });
     };
 }
 
